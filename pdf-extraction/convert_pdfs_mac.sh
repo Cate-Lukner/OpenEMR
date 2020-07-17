493c9c3f407d80c062765f393aa27cf724cc9b6d
Cate-Lukner <catluk32@gmail.com>
2020-07-16 20:41:24 -0500

    Created a version for mac

---
+++ pdf-extraction/convert_pdfs_mac.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Get all the PDF files
+FILES=$(find . -type f -name "*.pdf")
+
+# Loop through all the PDF files
+for file in $FILES
+do
+	# Get the basename of the file
+	filename=$(basename "$file" .pdf)
+
+	# Convert to an image
+	gconvert -density 300 $file -depth 8 -strip -background white -alpha off $filename.tiff
+	# Use tesseract to conver to a .txt document
+	gtesseract $filename.tiff $filename
+done
+
+
+# Clean up the uneeded .tiff files
+for file in $FILES
+do
+	filename=$(basename "$file" .pdf)
+	rm $filename.tiff
+done
+'
+
