58d15c406266d2634fde9079fc967a46b23c5dc3
Cate-Lukner <catluk32@gmail.com>
2020-07-16 21:14:19 -0500

    Make it convert only one PDF

--- pdf-extraction/mac_convert_pdfs.sh
+++ pdf-extraction/mac_convert_pdfs.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
 # Get all the PDF files
-FILES=$(find . -type f -name "*.pdf")
+# FILES=$(find . -type f -name "*.pdf")
+FILE1=$1
 
 # Loop through all the PDF files
-for file in $FILES
-do
-	# Get the basename of the file
-	filename=$(basename "$file" .pdf)
+# for file in $FILES
+# do
+# Get the basename of the file
+filename=$(basename "$FILE1" .pdf)
 
-	# Convert to an image
-	magick convert -density 300 $file -depth 8 -strip -background white -alpha off $filename.tiff
-	# Use tesseract to conver to a .txt document
-	tesseract $filename.tiff $filename
-done
+# Convert to an image
+magick convert -density 300 $FILE1 -depth 8 -strip -background white -alpha off $filename.tiff
+# Use tesseract to conver to a .txt document
+tesseract $filename.tiff $filename
+# done
 
 
 # Clean up the uneeded .tiff files
@@ -22,5 +23,5 @@ do
 	filename=$(basename "$file" .pdf)
 	rm $filename.tiff
 done
-'
+
 
